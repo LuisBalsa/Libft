c069a0745e611707558ce042a8b17efddb48cb67
Luis Filipe De Sousa Balsa <luide-so@c1r6s7.42porto.com>
2023-04-20 15:22:55 +0100

    15:22 42
    Varias funcoes novas

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: luide-so <luide-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 15:38:49 by luide-so          #+#    #+#             */
-/*   Updated: 2023/04/19 19:01:24 by luide-so         ###   ########.fr       */
+/*   Updated: 2023/04/20 14:57:27 by luide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,8 @@ int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
